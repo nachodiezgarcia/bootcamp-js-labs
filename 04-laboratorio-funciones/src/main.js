5403306d19380751f7f1febb73e7b2fa52e1ec07
Nacho Diez <diezgarcianacho@gmail.com>
2024-09-04 17:59:44 +0200

    Avanzamos más tarde

---
+++ 04-laboratorio-funciones/src/main.js
@@ -0,0 +1,9 @@
+function CambioDeTurno () {
+
+};
+
+const siguiente = document.getElementById("Tsiguiente");
+const anterior = document.getElementById("Tanterior");
+
+siguiente.addEventListener("click", CambioDeTurno);
+anterior.addEventListener("click", CambioDeTurno);
